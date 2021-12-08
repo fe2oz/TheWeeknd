70b4b546b8bc204bd71a677182dee67f9e03127b
fe2oz <93234748+fe2oz@users.noreply.github.com>
2021-12-09 00:22:10 +0900

    12.09/00:22

--- js/album_slide.js
+++ js/album_slide.js
@@ -1,16 +1,22 @@
+
 $(function(){
-    $('.prev').click(function(){
-            $('#ho-zon-list li:last').prependTo('#ho-zon-list');
-            $('#ho-zon-list').css('margin-left',-1804);
-            $('#ho-zon-list').stop().animate({marginLeft:0},800)
-    });
-        
-    $('.next').click(function(){
-            $('#ho-zon-list').stop().animate({marginLeft:-1804},800, function(){
-            $('#ho-zon-list li:first').appendTo('#ho-zon-list');
-            $('#ho-zon-list').css({marginLeft:0});
-            });
+        document.getElementById('ss_1').onmouseover=function(){
+                const cimg = document.getElementById('ss_1mage');
+                cimg.src = "img/main/TMB3.jpg"
+        }
 
-    });
+        document.getElementById('ss_1').onmouseleave=function(){
+                const cimg = document.getElementById('ss_1mage');
+                cimg.src = "img/main/TMB1.jpg"
+        }
 
-});
+        document.getElementById('ss_2').onmouseover=function(){
+                const cimg = document.getElementById('ss_2mage');
+                cimg.src = "img/main/PM-TW-ORN-COVER-(LAYERS)_03.jpg"
+        }
+
+        document.getElementById('ss_2').onmouseleave=function(){
+                const cimg = document.getElementById('ss_2mage');
+                cimg.src = "img/main/PM-TW-ORN-COVER-(LAYERS)_02.jpg"
+        }
+})
