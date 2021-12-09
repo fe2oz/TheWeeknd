49d10476bad8afaf41887e919b6b23470fa0f455
fe2oz <93234748+fe2oz@users.noreply.github.com>
2021-12-09 17:42:50 +0900

    12.09/17:42

---
+++ js/header.js
@@ -0,0 +1,17 @@
+
+
+
+$(function(){
+    $('#logo_menu').click(function(){
+        $(".large").slideToggle("slow");
+    })
+    $("#close_menu").click(function(){
+        $(".large").slideToggle("slow");
+    })
+})
+
+$(window).resize(function(){
+    if(window.innerWidth>=1000){
+        $('.main_menu').show();
+    }
+});
