513afa84a7f0c5508b38460a762703322b861bd7
fe2oz <93234748+fe2oz@users.noreply.github.com>
2022-02-10 10:07:07 +0900

    02.10/10:07

--- js/header.js
+++ js/header.js
@@ -4,9 +4,11 @@
 $(function(){
     $('#logo_menu').click(function(){
         $(".large").slideToggle("slow");
+        $("#close_menu").css("display", "block")
     })
     $("#close_menu").click(function(){
         $(".large").slideToggle("slow");
+        $(this).css("display", "none")
     })
 })
 
